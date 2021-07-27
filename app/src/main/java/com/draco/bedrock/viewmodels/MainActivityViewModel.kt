62fb38438319be6df9db2b97dd104f3eb7fc5698
Tyler Nijmeh <tylernij@gmail.com>
2021-07-26 21:15:17 -0700

    Clean things up; move logic to VM
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- app/src/main/java/com/draco/bedrock/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/draco/bedrock/viewmodels/MainActivityViewModel.kt
@@ -1,7 +1,56 @@
 package com.draco.bedrock.viewmodels
 
 import android.app.Application
+import androidx.documentfile.provider.DocumentFile
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.draco.bedrock.repositories.constants.GoogleDriveFiles
+import com.draco.bedrock.repositories.remote.GoogleAccount
+import com.draco.bedrock.repositories.remote.GoogleDrive
+import com.draco.bedrock.utils.DocumentFileZip
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
+    val googleAccount = GoogleAccount(application.applicationContext)
+    var googleDrive: GoogleDrive? = null
+
+    /**
+     * Initialize the Google Drive instance
+     */
+    fun initGoogleDrive() {
+        val context = getApplication<Application>().applicationContext
+
+        googleAccount.account?.let {
+            googleDrive = GoogleDrive(context, it)
+        }
+    }
+
+    /**
+     * Upload the Google Drive world file
+     */
+    fun uploadWorldsDriveFile(root: DocumentFile) {
+        val context = getApplication<Application>().applicationContext
+
+        viewModelScope.launch {
+            val zipBytes = DocumentFileZip(context, root).zip()
+            googleDrive?.createFileIfNecessary(GoogleDriveFiles.cloudWorldZip)
+            googleDrive?.writeFileBytes(GoogleDriveFiles.cloudWorldZip, zipBytes)
+        }
+    }
+
+    /**
+     * Extracts the Google Drive world file
+     */
+    fun extractWorldsDriveFile(root: DocumentFile) {
+        val context = getApplication<Application>().applicationContext
+
+        viewModelScope.launch(Dispatchers.IO) {
+            if (googleDrive?.fileExists(GoogleDriveFiles.cloudWorldZip) == true) {
+                val zipBytes = googleDrive?.readFileBytes(GoogleDriveFiles.cloudWorldZip)?.let {
+                    DocumentFileZip(context, root).unZip(it)
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/draco/bedrock/views/MainActivity.kt
+++ app/src/main/java/com/draco/bedrock/views/MainActivity.kt
@@ -1,53 +1,28 @@
 package com.draco.bedrock.views
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.activity.viewModels
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.documentfile.provider.DocumentFile
 import com.draco.bedrock.databinding.ActivityMainBinding
-import com.draco.bedrock.repositories.constants.GoogleDriveFiles
-import com.draco.bedrock.repositories.remote.GoogleAccount
-import com.draco.bedrock.repositories.remote.GoogleDrive
-import com.draco.bedrock.utils.DocumentFileZip
 import com.draco.bedrock.viewmodels.MainActivityViewModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainActivityViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
 
-    private val googleAccount: GoogleAccount by lazy { GoogleAccount(this) }
-    private val googleDrive: GoogleDrive by lazy { GoogleDrive(this, googleAccount.account!!) }
-
-    private lateinit var zipBytes: ByteArray
+    private lateinit var documentFile: DocumentFile
 
     private val explicitLoginHandler = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        googleAccount.handleExplicitSignIn(it)
+        viewModel.googleAccount.handleExplicitSignIn(it)
     }
 
     private val treeHandler = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         val uri = it.data!!.data!!
-        val documentFile = DocumentFile.fromTreeUri(this, uri)
-
+        documentFile = DocumentFile.fromTreeUri(this, uri)!!
         contentResolver.takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-
-        val documentFileZip = DocumentFileZip(this, documentFile!!).zip()
-        Log.d("DONE!", documentFileZip.size.toString())
-
-        GlobalScope.launch {
-            //googleDrive.createFileIfNecessary(GoogleDriveFiles.cloudWorldZip)
-            //googleDrive.writeFileBytes(GoogleDriveFiles.cloudWorldZip, documentFileZip)
-
-            //documentFile.listFiles().forEach { l ->
-            //    l.delete()
-            //}
-
-            DocumentFileZip(this@MainActivity, documentFile).unZip(zipBytes)
-        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,24 +31,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.googleSignIn.setOnClickListener {
-            googleAccount.discoverAccountExplicit(explicitLoginHandler)
+            viewModel.googleAccount.discoverAccountExplicit(explicitLoginHandler)
         }
 
         binding.googleDriveSignIn.setOnClickListener {
-            googleDrive.requestPermissionsIfNecessary(this)
-
-            GlobalScope.launch {
-                if (googleDrive.fileExists(GoogleDriveFiles.cloudWorldZip)) {
-                    zipBytes = googleDrive.readFileBytes(GoogleDriveFiles.cloudWorldZip)
-                    Log.d("EXISTING", zipBytes.size.toString())
-                }
-            }
+            viewModel.initGoogleDrive()
+            viewModel.googleDrive?.requestPermissionsIfNecessary(this)
         }
 
         binding.saf.setOnClickListener {
             val intent = Intent()
                 .setAction(Intent.ACTION_OPEN_DOCUMENT_TREE)
             treeHandler.launch(intent)
+        }
+
+        binding.upload.setOnClickListener {
+            viewModel.uploadWorldsDriveFile(documentFile)
+        }
+
+        binding.download.setOnClickListener {
+            viewModel.extractWorldsDriveFile(documentFile)
         }
     }
 }
